13bff79c7b58ddf1afed7cbc324fd6ef86ce2744
unknown <araenicthea@gmail.com>
2023-11-09 13:21:24 +0800

    added hatespeech overview

--- thesis-website/components/Overview.jsx
+++ thesis-website/components/Overview.jsx
@@ -1,34 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase';
 import { Doughnut } from 'react-chartjs-2';
-import {Chart, ArcElement} from 'chart.js'
-Chart.register(ArcElement);
 
-const Overview = () => {
+const HateSpeechOverview = () => {
+  const [hateSpeechData, setHateSpeechData] = useState([]);
+  const [totalContent, setTotalContent] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  let totalHateSpeech = 0;
+
+  async function fetchData() {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'drafts'));
+      const hateSpeechData = [];
+
+      querySnapshot.forEach((doc) => {
+        const entry = doc.data();
+        hateSpeechData.push(entry);
+
+        if (entry.isHateSpeech) {
+          totalHateSpeech += 1;
+        }
+      });
+
+      setHateSpeechData(hateSpeechData);
+      setTotalContent(querySnapshot.size);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const data = {
-    labels: ['Positive', 'Neutral', 'Negative'],
+    labels: ['Non-Hate Speech', 'Hate Speech'],
     datasets: [
       {
-        data: [40, 30, 30],
-        backgroundColor: ['rgba(75, 192, 192, 0.5)', 'rgba(255, 206, 86, 0.5)', 'rgba(255, 99, 132, 0.5)'],
+        data: [totalContent - totalHateSpeech, totalHateSpeech],
+        backgroundColor: ['rgba(75, 192, 192, 0.5)', 'rgba(255, 99, 132, 0.5)'],
       },
     ],
   };
 
-  const options = {
-    cutout: '70%',
-    plugins: {
-      legend: {
-        position: 'bottom',
-      },
-    },
-  };
-
   return (
     <div>
-      <h2 class="font-bold text-xl text-center m-5">Overview</h2>
-      <Doughnut data={data} options={options} />
+      <h2>Hate Speech Overview</h2>
+      {loading ? (
+        <p>Loading data...</p>
+      ) : error ? (
+        <p>An error occurred while fetching data: {error.message}</p>
+      ) : (
+        <div>
+          <Doughnut data={data} />
+          <p>Total Content: {totalContent}</p>
+          <p>Total Hate Speech Incidents: {totalHateSpeech}</p>
+          <p>Hate Speech Percentage: {((totalHateSpeech / totalContent) * 100).toFixed(2)}%</p>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Overview;
+export default HateSpeechOverview;
